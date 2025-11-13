bad4fee74dcef9b88b4e1cace5dca3e00dff9e22
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-13 09:38:17 +0900

    인터페이스 문법

--- src/d/inheritance/InterfaceMain.java
+++ src/d/inheritance/InterfaceMain.java
@@ -1,4 +1,65 @@
 package d.inheritance;
 
+abstract class Animal5 {
+    abstract void run();
+    abstract void sleep();
+    abstract void eat();
+}
+
+class Dog5 extends Animal5 {
+    void run() {}
+    void sleep() {}
+    void  eat() {}
+}
+
+interface Animal6 {
+    void run();
+    void sleep();
+    void eat();
+}
+
+class Dog6 implements Animal6 {
+    @Override
+    public void run() {
+        System.out.println("Dog run");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Dog sleep");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog eat");
+    }
+}
+
+interface Drawable {
+    void draw();
+}
+
+
+class Rectangle6 implements Drawable {
+    private int width, height;
+
+    Rectangle6(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("직사각형 그리기: " + width + "x" + height);
+    }
+
+}
+
 public class InterfaceMain {
+    public static void main(String[] args) {
+        Animal6 d= new Dog6();
+        d.run();
+        d.sleep();
+        d.eat();
+    }
 }
--- src/d/inheritance/MultiInterface.java
+++ src/d/inheritance/MultiInterface.java
@@ -1,4 +1,45 @@
 package d.inheritance;
 
+class Animal7 {
+    void eat() {
+        System.out.println("eating");
+    }
+}
+
+interface Swimmable {
+    void swim();
+}
+
+interface Flyable {
+    void fly();
+}
+
+interface Walkable {
+    void walk();
+}
+
+class Duck implements Swimmable, Flyable, Walkable {
+    @Override
+    public void fly() {
+        System.out.println("오리가 날아갑니다");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("오리가 수영합니다");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("오리가 걸어다닙니다");
+    }
+}
+class Fish implements Swimmable {
+    @Override
+    public void swim() {
+        System.out.println("물고기가 수영합니다");
+    }
+}
+
 public class MultiInterface {
 }
